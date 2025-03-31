0828bef96861692490aa6779d1f11cb1b2df57f8
Patrik Skogberg <skogbergp@gmail.com>
2025-03-31 12:47:03 +0300

    Add development script and implement cat API endpoint

--- app.js
+++ app.js
@@ -10,3 +10,16 @@ app.get('/', (req, res) => {
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+app.get('/api/v1/cat', (req, res) => {
+  const cat = {
+    id: 1,
+    name: 'Fluffy',
+    birthdate: '2020-01-01',
+    weight: 4.5,
+    owner: 'John Doe',
+    Image: 'https://loremflickr.com/320/240/cat',
+  };
+  res.send(cat);
+});
+
+app.use('/public', express.static('public'));
